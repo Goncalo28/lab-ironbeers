76090570efc8999206840573a1d2b53e914d7f45
Goncalo Marques <goncalomarques9@hotmail.com>
2020-11-03 18:01:37 +0000

    initial commit

--- app.js
+++ app.js
@@ -8,18 +8,42 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()
+  .then((beers) => {
+    res.render('beers', {beers: beers})
+  })
+  .catch((error) => {
+    console.log('error', error);
+  })
+
+})
+
+app.get('/beers-random', (req, res) => {
+
+  punkAPI.getRandom()
+  .then((randomBeers) => {
+    res.render('randomBeers', {randomBeers: randomBeers})
+  })
+  .catch((error) => {
+    console.log('error', error);
+  })
+  
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
